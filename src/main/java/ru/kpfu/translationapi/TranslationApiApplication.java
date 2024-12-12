76844fcc6c66a731eb1d4113d366a98e13716e14
FkishDaniels <irina2808777@gmail.com>
2024-12-12 15:11:27 +0300

    Initial commit

---
+++ src/main/java/ru/kpfu/translationapi/TranslationApiApplication.java
@@ -0,0 +1,13 @@
+package ru.kpfu.translationapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TranslationApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TranslationApiApplication.class, args);
+    }
+
+}
